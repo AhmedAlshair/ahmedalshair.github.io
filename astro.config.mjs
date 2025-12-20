6d455b8ca23ad4b7df7d294fa3ce805a0ad5184b
aalshair <aalshair@cometapp.net>
2025-12-20 16:11:26 +0800

    Added custom domain settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  site: 'https://ahmedalshair.github.io',
+  site: 'https://ahmedlalshair.net',
 });
